24248b51b705c10c5a22e4256761dbdbdf4415fc
Jesús María Luis Gil <inteclu@gmail.com>
2015-03-22 13:26:16 +0100

    dorpbox again

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,10 +1,10 @@
 class Image < ActiveRecord::Base
   do_not_validate_attachment_file_type :newimg
-  has_attached_file :newimg, :styles => { :high => "1024x104", :medium => "600x600>", :thumb => "250x250>" }#,
-  					# :storage => :dropbox,
-       #              :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-       #              :dropbox_options => {},
-       #              :path => "new/:style/:id_:filename"
+  has_attached_file :newimg, :styles => { :high => "1024x1024", :medium => "600x600>", :thumb => "250x250>" },
+  					 :storage => :dropbox,
+                     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                     :dropbox_options => {},
+                     :path => "new/:style/:id_:filename"
   
 
   belongs_to :article
