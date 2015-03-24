eaeb21c63835d11761d9c1d6d742fcd82e1f7d37
Jesús María Luis Gil <inteclu@gmail.com>
2015-03-24 18:29:33 +0100

    cambios mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,12 +41,12 @@ Mybaby::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
+  address:              "smtp.gmail.com",
   port:                 587,
-  domain:               'gmail.com',
+  domain:               "gmail.com",
   user_name:            "<%= ENV['MYBABYBLOG_EMAIL'] %>",
   password:             "<%= ENV['MYBABYBLOG_EMAIL_PASSWORD'] %>",
-  authentication:       'plain',
+  authentication:       "plain",
   enable_starttls_auto: true  }
   
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
