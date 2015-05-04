ff94736ef376c723ce96a6b0bb369819cad4e440
Jesús María Luis Gil <inteclu@gmail.com>
2015-05-04 16:50:09 +0200

    added meta tags and meta description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,23 @@
 module ApplicationHelper
-  SECRET_WORD="hugolm"
 
   def glyphicon(icon)
     "<span class='#{icon}' aria-hidden='true'></span>".html_safe
   end
+
+  def meta_keywords(tags = nil)
+    if tags.present?
+      content_for :meta_keywords, tags
+    else
+      content_for?(:meta_keywords) ? [content_for(:meta_keywords), APP_CONFIG['meta_keywords']].join(', ') : APP_CONFIG['meta_keywords']
+    end
+  end
+
+  def meta_description(desc = nil)
+    if desc.present?
+      content_for :meta_description, desc
+    else
+      content_for?(:meta_description) ? content_for(:meta_description) : APP_CONFIG['meta_description']
+    end
+  end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Mybaby::Application.initialize!
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")
 
 #config.gem 'tiny_mce'
